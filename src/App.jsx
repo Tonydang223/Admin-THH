f6701df9f6461d7c9d14f59385c82b8e9d946390
Tonydang223 <danghnh@dipro.vn>
2023-12-09 20:59:42 +0700

    new-one

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss";
+import AuthMiddleware from "./components/ProtectedRoutes/AuthMidlleware";
+import MainPages from "./pages";
+
+function App() {
+  return (
+    <>
+      <AuthMiddleware>
+        <MainPages />
+      </AuthMiddleware>
+    </>
+  );
+}
+
+export default App;
